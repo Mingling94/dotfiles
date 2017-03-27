2818e913b84337804091e1cc770105a9bc9e883b
Ming Luo <mcluo1994@gmail.com>
2017-03-26 22:32:17 -0700

    Better wifi refresh script

--- bin/refresh-wifi.sh
+++ bin/refresh-wifi.sh
@@ -2,5 +2,10 @@
 # Restarts the network-manager service to reconnect to wifi signal
 # Generally attach to Ctrl+Alt+W
 
-echo "FILL_IN_PW" | sudo -S service network-manager restart \
-	&& echo "Network manager restarting..."
+if [ $(rfkill list wifi | grep "Soft blocked: yes" | wc -l) -gt 0 ] ; then
+    rfkill unblock wifi
+    zenity --info --text "Enabled wireless" --timeout="1"
+else
+    rfkill block wifi
+    zenity --info --text "Disabled wireless" --timeout="1"
+fi
